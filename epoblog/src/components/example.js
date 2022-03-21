b6236d575c64e26861cc5c9e8d22c0a5e7e24152
epopisces <epopisces@gmail.com>
2022-03-21 14:01:15 -0500

    markdown working, mermaid is not working

---
+++ epoblog/src/components/example.js
@@ -0,0 +1,15 @@
+export default `
+# markdown-it-vue
+# This is a test
+## Heading 2
+_lorem ipsum_ etc etc etc
+
+\`\`\`mermaid
+graph TD;
+    A-->B;
+    A-->C;
+    B-->D;
+    C-->D;
+\`\`\`
+
+`
